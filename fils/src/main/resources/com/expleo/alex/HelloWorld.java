636ee3a7595065c129a6e58bfb50163c7f285db1
Alex HIER <ahier@assystemtechnologies.com>
2019-04-09 16:32:12 +0200

    commit test jenkins

---
+++ fils/src/main/resources/com/expleo/alex/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.expleo.alex;
+
+public class HelloWorld {
+	
+private void psvm() {
+	// TODO Auto-generated method stub
+System.out.print("Félicitation mon gars!!!! Tu es le meilleur ;-)");
+}
+}
